03df19be3ad30e78d56b16dc35f25c6c9b2ac7a3
Varun Rao <varun21exps@gmail.com>
2018-02-15 18:01:19 +0530

    Landing Pages skeleton made. Text added and little bit beautification added.

--- currency-converter/src/app/landing-page/landing-page.component.ts
+++ currency-converter/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AppComponent } from '../app.component'
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -11,5 +11,5 @@ export class LandingPageComponent implements OnInit {
 
   ngOnInit() {
   }
-  title = AppComponent.title
+  title = 'Currency Converter';
 }
