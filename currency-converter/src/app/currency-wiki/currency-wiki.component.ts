57f731410c0f04d458b827df09051f251247cf6f
Varun Rao <varun21exps@gmail.com>
2018-03-13 23:06:44 +0530

    Currency Info loaded from JSON. Data binding applied. Now the currency info can be viewed. Styling still left.

--- currency-converter/src/app/currency-wiki/currency-wiki.component.ts
+++ currency-converter/src/app/currency-wiki/currency-wiki.component.ts
@@ -9,6 +9,15 @@ import { CurrCodeServiceService } from '../curr-code-service.service';
 })
 export class CurrencyWikiComponent implements OnInit {
   currencies: Currency[] = [];
+  selected_currency: Currency;
+  chosen_currency_number: string;
+  chosen_currency_major_units: string;
+  chosen_currency_minor_units: string; 
+  chosen_currency_freq_bn: string;
+  chosen_currency_rare_bn: string;
+  chosen_currency_freq_cn: string;
+  chosen_currency_rare_cn: string;
+
   constructor(private currency_code_service: CurrCodeServiceService)
   {
 
@@ -36,4 +45,41 @@ export class CurrencyWikiComponent implements OnInit {
                              }});
   }
 
+  getCurrencyInfo(): void
+  {
+    this.currency_code_service.loadCurrencyInfo()
+                           .subscribe(result =>{
+                              this.chosen_currency_freq_bn = "";
+                              this.chosen_currency_freq_cn = "";
+                              this.chosen_currency_rare_bn = "";
+                              this.chosen_currency_rare_cn = "";
+                              this.chosen_currency_number = result[this.selected_currency.code].iso.number;
+                              this.chosen_currency_major_units = result[this.selected_currency.code].units.major.name+" ("+result[this.selected_currency.code].units.major.symbol+")";
+                              this.chosen_currency_minor_units = result[this.selected_currency.code].units.minor.name+" ("+result[this.selected_currency.code].units.minor.symbol+")";
+                              for(var i=0;i<result[this.selected_currency.code].banknotes.frequent.length;i++)
+                              {
+                                this.chosen_currency_freq_bn += result[this.selected_currency.code].banknotes.frequent[i];
+                                if(i!=result[this.selected_currency.code].banknotes.frequent.length-1)
+                                  this.chosen_currency_freq_bn += ", ";
+                              }
+                              for(var i=0;i<result[this.selected_currency.code].banknotes.rare.length;i++)
+                              {
+                                this.chosen_currency_rare_bn += result[this.selected_currency.code].banknotes.rare[i];
+                                if(i!=result[this.selected_currency.code].banknotes.rare.length-1)
+                                  this.chosen_currency_rare_bn += ", ";
+                              }
+                              for(var i=0;i<result[this.selected_currency.code].coins.frequent.length;i++)
+                              {
+                                this.chosen_currency_freq_cn += result[this.selected_currency.code].coins.frequent[i];
+                                if(i!=result[this.selected_currency.code].coins.frequent.length-1)
+                                  this.chosen_currency_freq_cn += ", ";
+                              }
+                              for(var i=0;i<result[this.selected_currency.code].coins.rare.length;i++)
+                              {
+                                this.chosen_currency_rare_cn += result[this.selected_currency.code].coins.rare[i];
+                                if(i!=result[this.selected_currency.code].coins.rare.length-1)
+                                  this.chosen_currency_rare_cn += ", ";
+                              }
+                            });
+  }
 }
