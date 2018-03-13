0080ab75a6c3dc7cf53e37a21aaab010f5905510
Varun Rao <varun21exps@gmail.com>
2018-03-13 22:08:57 +0530

    Currency Code service used to fetch the codes and the names of the currencies. Select menu filled with all the currencies now.

--- currency-converter/src/app/currency-wiki/currency-wiki.component.ts
+++ currency-converter/src/app/currency-wiki/currency-wiki.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Currency } from '../currency';
+import { CurrCodeServiceService } from '../curr-code-service.service';
 
 @Component({
   selector: 'app-currency-wiki',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency-wiki.component.css']
 })
 export class CurrencyWikiComponent implements OnInit {
+  currencies: Currency[] = [];
+  constructor(private currency_code_service: CurrCodeServiceService)
+  {
 
-  constructor() { }
+  }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    this.getCurrencyCodes();
+  }
+
+  getCurrencyCodes(): void
+  {
+      /**
+      * This code gets the currency codes from the CurrCodeServiceService, and
+      * puts them into the currencies array.
+      *
+      * @param None
+      * @return None
+      */
+      this.currency_code_service.loadCurrencyCodes()
+                           .subscribe(result =>{
+                             for(var i=0;i<Object.keys(result).length;i++)
+                             {
+                               this.currencies.push(new Currency(result[Object.keys(result)[i]].code,result[Object.keys(result)[i]].name))
+                             }});
   }
 
 }
